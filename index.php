c7039f033cf57053336cf3517b233b797560b03a
Lester Covey <me@lestercovey.ml>
2022-12-30 17:11:55 +0300

    Added recheck command, db integration

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
+ini_set('display_errors', 1);
 $rec = file_get_contents('php://input');
 $data = json_decode($rec, true);
 
-if (empty($data['message']['chat']['id'])) {
+if (empty($data['message']['chat']['id']) || empty($data['message']['text'])) {
 	exit();
 }
 
 $text = $data['message']['text'];
 $user = $data['message']['chat']['id'];
 define('TOKEN', '5864116167:AAG5ghXa50RI9Dm2_uhcmZMQVQ-hhv9Ei64');
+define('CLOSING', "\n❇️Подписывайся на канал: @cockhunter");
 define('TEXT', $data['message']['text']);
 define('SENDER', $data['message']['chat']['id']);
 
@@ -23,7 +25,7 @@ function sendTelegram($method, $response) {
 	return $res;
 }
 
-function q($text) {
+function q($text, $die=false) {
 	sendTelegram(
 		'sendMessage',
 		array(
@@ -33,29 +35,59 @@ function q($text) {
 			'disable_web_page_preview' => true
 		)
 	);
+	if ($die) {
+		exit(0);
+	}
 }
 
-if (TEXT == "/start") {
-	q("Привет! Я помогу узнать предварительную оценку проекта. Отправь ссылку на проект в формате https://portfolio.hse.ru/Project/159642");
-} else {
-	$split = explode("/", TEXT);
-	if (count($split) < 2) {
-		q("Не похоже на ссылку");
+function check($id) {
+	$link = 'https://portfolio.hse.ru/Project/ProjectDataForViewer?userProjectId=' . $id;
+	$data = json_decode(file_get_contents($link), true);
+	if ($data == null) {
+		q('Проект не найден', true);
 	} else {
-		$link = "https://portfolio.hse.ru/Project/ProjectDataForViewer?userProjectId=" . end($split);
-		$data = json_decode(file_get_contents($link), true);
-		if ($data == null) {
-			q("Проект не найден");
+		if (!isset($data['title'])) {
+			q('Не удалось получить проект', true);
 		} else {
-			if (!isset($data['title'])) {
-				q("Не удалось получить проект");
+			if (!isset($data['totalMark'])) {
+				q($data['title'] . "\nОценка недоступна");
 			} else {
-				if (!isset($data['totalMark'])) {
-					q($data['title'] . "\nОценка недоступна");
-				} else {
-					q($data['title'] . "\nОценка: " . $data['totalMark'] . "\n❇️Подписывайся на канал: @cockhunter");
-				}
+				q($data['title'] . "\nОценка: " . $data['totalMark'] . CLOSING);
 			}
 		}
+	}
+}
+
+function exceptions_error_handler($severity, $message, $filename, $lineno) {
+	q('Error: ' . $message . ' @' . $lineno);
+        throw new ErrorException($message, 0, $severity, $filename, $lineno);
+}
+set_error_handler('exceptions_error_handler', E_ALL);
+
+$db = new SQLite3('/var/www/premark_core/db.db');
+$s = $db->prepare('SELECT act FROM interactions WHERE id = :id;');
+$s->bindValue(':id', SENDER);
+$act = $s->execute();
+$act = $act->fetchArray()[0];
+
+if (TEXT == '/start') {
+	q('Привет! Я помогу узнать предварительную оценку проекта. Отправь ссылку на проект в формате https://portfolio.hse.ru/Project/159642');
+} else if (TEXT == '/recheck') {
+	if (is_null($act)) {
+		q('Проект пока не проверялся');
+	} else {
+	check($act);
+	}
+} else {
+	$split = explode('/', TEXT);
+	if (count($split) < 2) {
+		q('Не похоже на ссылку');
+	} else {
+		$id = end($split);
+		check($id);
+		$s = $db->prepare('REPLACE INTO interactions(id, act) VALUES(:id, :act);');
+		$s->bindValue(':id', SENDER);
+		$s->bindValue(':act', $id);
+		$s->execute();
 	}
 }
